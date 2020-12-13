963f1b6a13bdc7ee59c08b7cfcd76674a16b06ba
Doryan Dahon <ddahon@gmail.com>
2020-12-13 12:19:43 +0100

    Sérialization de KV, fix de la modification du nom de fichier dans HdfsRead

---
+++ hidoop/src/formats/KVS.java
@@ -0,0 +1,24 @@
+package formats;
+
+import java.io.Serializable;
+
+public class KVS implements Serializable{
+
+	public static final String SEPARATOR = "<->";
+	
+	public String k;
+	public String v;
+	
+	public KVS() {}
+	
+	public KVS(String k, String v) {
+		super();
+		this.k = k;
+		this.v = v;
+	}
+
+	public String toString() {
+		return "KV [k=" + k + ", v=" + v + "]";
+	}
+	
+}
