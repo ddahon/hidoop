2427ea9a5476308dca44537ff9b8a5fbfb3474ad
naoutix <naoutix@gmail.com>
2021-01-21 23:25:52 +0100

    thread

--- src/hdfs/HdfsThread.java
+++ src/hdfs/HdfsThread.java
@@ -1,14 +1,17 @@
 package hdfs;
 import java.io.File;
+import java.net.Socket;
 public class HdfsThread extends Thread {
-    
+    private Socket mySocket;
     private Message message;
-    public HdfsThread (Message mes){
+    public HdfsThread (Message mes,Socket s){
         this.message = mes;
+        this.mySocket = s;
     }
 
     public void run(){
         File f = new File(this.message.getPremierNomFichier());
         f.delete();
+        mySocket.close();
     }
 }
