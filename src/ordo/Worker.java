7cb2123d9e301a478bfd45be4bfa7707a835e00d
Joceran <joceran.g@gmail.com>
2021-01-21 22:37:06 +0100

    Worker

--- src/ordo/Worker.java
+++ src/ordo/Worker.java
@@ -17,7 +17,7 @@ public class Worker extends UnicastRemoteObject implements WorkerInt {
         this.host = host;
         this.port = port;
         LocateRegistry.createRegistry(Integer.parseInt(port));
-        Naming.rebind("//" + host + ":" + port + "/worker", this);
+        Naming.rebind("//localhost/worker", this);
     }
 
     @Override
