2e975057423f316c6b14a447199db2f7df2a5aaa
Spadeon <joceran.g@gmail.com>
2021-01-21 23:38:15 +0100

    genaimare

--- src/config/Project.java
+++ src/config/Project.java
@@ -4,7 +4,7 @@ public class Project {
     public static final String PATH = "";
     public static final int nbNodes = 1;
     public static final String[] hosts = {"tao"};
-    public static final String[] ports = {"1251"};œ
+    public static final String[] ports = {"1251"};
     public static final String username = "ddahon";
     public static final String serverPathToHidoop = "2A/hidoop";
     public static final int tailleMaxEnvoi=10;
